2172a443e2bbb6e1dc999f0c12981c54388e80e2
zhangbihan999 <zhangbihan999@gmail.com>
2026-07-07 14:31:13 +0800

    pg17 gptuner water, tpcds

--- src/run/run.py
+++ src/run/run.py
@@ -47,13 +47,13 @@ def main():
         #"PYTHONPATH=src python3 src/run/WAter/run_gptuner.py -seed=858 | tee log858.txt",
 
         # water+gptuner 完全体
-        "PYTHONPATH=src python3 src/run/WAter/run_gptuner.py -seed=896 | tee log896.txt",
+        #"PYTHONPATH=src python3 src/run/WAter/run_gptuner.py -seed=896 | tee log896.txt",
 
         # water+smac 完全体
         #"PYTHONPATH=src python3 src/run/WAter/run_smac.py -seed=712 | tee log712.txt",
 
         # vanilla gptuner
-        #"PYTHONPATH=src python3 src/run/vanilla_tuner/run_gptuner.py -seed=829 | tee log829.txt",
+        "PYTHONPATH=src python3 src/run/vanilla_tuner/run_gptuner.py -seed=897 | tee log897.txt",
 
         # ∞
         #"PYTHONPATH=src python3 src/run/WAter/run_gptuner.py -seed=768 | tee log768.txt",
